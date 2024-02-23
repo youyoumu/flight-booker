6738d889b5ab8188a4ea852d131ea0447a1e5a4a
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-02-23 17:30:14 +0700

    Use airport code as link

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,7 +4,7 @@ class FlightsController < ApplicationController
     @flight_dates = Flight.all.map { |flight| [flight.departure_time.to_date.strftime('%d/%m/%Y'), flight.departure_time.to_date] }
     @flight_dates = @flight_dates.uniq
     if params[:search] == 'true'
-      @flights = Flight.where(
+      @flights = Flight.includes(:departure_airport, :arrival_airport).where(
         departure_airport_id: params[:departure_airport_id]).where(
           arrival_airport_id: params[:arrival_airport_id]).where(
             'DATE(departure_time) = ?', Date.parse(params[:departure_date])
