5cc76b2c4ae05d8e1a48d796775255d55a4b377e
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-02-23 16:22:54 +0700

    Setup querry for Flight search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,5 +3,12 @@ class FlightsController < ApplicationController
     @airport_codes = Airport.all.map { |airport| [airport.code, airport.id] }
     @flight_dates = Flight.all.map { |flight| [flight.departure_time.to_date.strftime('%d/%m/%Y'), flight.departure_time.to_date] }
     @flight_dates = @flight_dates.uniq
+    if params[:search] == 'true'
+      @flights = Flight.where(
+        departure_airport_id: params[:departure_airport_id]).where(
+          arrival_airport_id: params[:arrival_airport_id]).where(
+            'DATE(departure_time) = ?', Date.parse(params[:departure_date])
+          )
+    end
   end
 end
