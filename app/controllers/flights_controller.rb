e2d7000e31353b6390d7f8e984935d992fd0010a
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-02-23 12:27:36 +0700

    Fix duplicate flight_dates

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @airport_codes = Airport.all.map { |airport| [airport.code, airport.id] }
-    @flight_dates = Flight.all.map { |flight| flight.departure_time.strftime('%d/%m/%Y') }
+    @flight_dates = Flight.all.map { |flight| [flight.departure_time.to_date.strftime('%d/%m/%Y'), flight.departure_time.to_date] }
+    @flight_dates = @flight_dates.uniq
   end
 end
