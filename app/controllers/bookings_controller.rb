699f0c969bed2b1cd8c445ebbd9434f96b632e81
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-02-24 15:49:19 +0700

    Create new action for Bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,8 @@
 class BookingsController < ApplicationController
   def new
-
+    if Flight.find(params[:flight_id]).exists?
+      @flight = Flight.find(params[:flight_id])
+      @num_tickets = params[:num_tickets]
+    end
   end
 end
