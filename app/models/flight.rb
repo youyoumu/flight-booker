cabf45da4f5d0336a4450187e8049469c85159a3
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-02-24 15:35:21 +0700

    Update Flight assosiation

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -13,4 +13,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+  has_many :bookings
+  has_many :booking_passengers, through: :bookings
+  has_many :passengers, through: :booking_passengers
 end
