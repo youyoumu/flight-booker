11e799029b87b0b0243538e6de9550be47c7d484
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-02-24 17:09:38 +0700

    Prepare new action and new view for Bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,13 @@ class BookingsController < ApplicationController
   def new
     if Flight.exists?(params[:flight_id])
       @flight = Flight.find(params[:flight_id])
-      @num_tickets = params[:num_tickets]
+      @num_tickets = params[:num_tickets].to_i
+
+      @booking = Booking.new
+      @passengers = []
+      @num_tickets.times do
+        @passengers << Passenger.new
+      end
     end
   end
 end
