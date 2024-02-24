496d309211db7dce6ee6386bb08799c1dec09fc5
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-02-24 15:57:08 +0700

    Fix #exists bug

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   def new
-    if Flight.find(params[:flight_id]).exists?
+    if Flight.exists?(params[:flight_id])
       @flight = Flight.find(params[:flight_id])
       @num_tickets = params[:num_tickets]
     end
