a4b57ab4cfb810ffa5a0cc1cd7ed73033823ea13
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-02-26 15:57:29 +0700

    Setup create action for Booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,6 +10,21 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+  def show
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
