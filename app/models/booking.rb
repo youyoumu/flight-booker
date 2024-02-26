f7548afd599c2d5e845f896facc10d44670303a3
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-02-26 14:06:08 +0700

    Accept nested attribute for Passenger

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -11,4 +11,5 @@ class Booking < ApplicationRecord
   belongs_to :flight_id
   has_many :booking_passengers
   has_many :passengers, through: :booking_passengers
+  accepts_nested_attributes_for :passengers
 end
