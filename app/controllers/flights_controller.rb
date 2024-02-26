9f56f6aee8c74e9ad84757560596987ce790d64f
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-02-26 13:51:33 +0700

    Use select_collection helper

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,5 @@
 class FlightsController < ApplicationController
   def index
-    @airport_codes = Airport.all.map { |airport| [airport.code, airport.id] }
     @flight_dates = Flight.all.map { |flight| [flight.departure_time.to_date.strftime('%d/%m/%Y'), flight.departure_time.to_date] }
     @flight_dates = @flight_dates.uniq
     if params[:search] == 'true' && !params[:num_tickets].blank?
