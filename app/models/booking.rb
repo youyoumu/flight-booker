1119b2d93079983d33acfc39e24ad3870d144062
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-02-26 15:45:46 +0700

    Fix Booking model bug

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -8,7 +8,7 @@
 #  updated_at :datetime         not null
 #
 class Booking < ApplicationRecord
-  belongs_to :flight_id
+  belongs_to :flight
   has_many :booking_passengers
   has_many :passengers, through: :booking_passengers
   accepts_nested_attributes_for :passengers
