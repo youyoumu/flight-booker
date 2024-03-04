df1054f9ede315a4df030646f53a8f7aaca03ded
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-03-04 23:27:53 +0700

    Messing around with stimulus

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  static targets = ["textArea", "length", "warning"]
+  
+  // connect() {
+  //   this.element.textContent = "Hello World!ss"
+  // }
+
+  check() {
+    if (this.textAreaTarget.value.length > 10) {
+      // this.textAreaTarget.value = this.textAreaTarget.value
+      this.warningTarget.style.display = "block"
+    }
+    else {
+      this.warningTarget.style.display = "none"
+    }
+    this.lengthTarget.textContent = this.textAreaTarget.value.length
   }
 }
