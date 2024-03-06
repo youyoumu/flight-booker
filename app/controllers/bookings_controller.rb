459cd76a7ab99e2e949d37c6fac0f6093fd9ed1f
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-03-06 17:52:14 +0700

    WIP

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,14 +15,12 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     if @booking.save
       redirect_to booking_path(@booking)
+    elsif Flight.exists?(params[:booking][:flight_id])
+      @flight = Flight.find(params[:booking][:flight_id])
+      @num_tickets = params[:booking][:num_tickets].to_i
+      render :new, status: :unprocessable_entity
     else
-      if Flight.exists?(params[:booking][:flight_id])
-        @flight = Flight.find(params[:booking][:flight_id])
-        @num_tickets = params[:booking][:num_tickets].to_i
-        render :new, status: :unprocessable_entity
-      else
-        redirect_to flights_path
-      end
+      redirect_to flights_path
     end
   end
 
@@ -35,9 +33,18 @@ class BookingsController < ApplicationController
     end
   end
 
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to booking_path(@booking)
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def booking_params
-    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
+    params.require(:booking).permit(:id, :flight_id, passengers_attributes: %i[name email])
   end
 end
