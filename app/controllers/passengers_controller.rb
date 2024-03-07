542c4a89e85f454ba0c8760024f6850eaf361b9a
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-03-07 13:36:39 +0700

    Clean unused passengers controller

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,24 +1,2 @@
 class PassengersController < ApplicationController
-  def new
-    @passenger = Passenger.new
-  end
-
-  def create
-    @passenger = Passenger.new
-    booking = Booking.find(params[:bookings][:id].to_i)
-    @passenger.bookings << booking
-    @passenger.assign_attributes(passenger_params)
-
-    if @passenger.save
-      redirect_to booking_path(booking)
-    else
-      # to do
-    end
-  end
-
-  private
-
-  def passenger_params
-    params.require(:passenger).permit(:name, :email, bookings_attributes: [:id])
-  end
 end
